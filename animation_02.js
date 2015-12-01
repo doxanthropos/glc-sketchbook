0f457ea28e08b00c6602cb60902612a77569628f
Knut Jackowski <doxanthropos@googlemail.com>
2015-12-01 09:15:54 +0100

    A simple animation, that is similar to one that I did decades ago in Amiga Basic

--- animation_02.js
+++ animation_02.js
@@ -3,7 +3,7 @@ function onGLC(glc) {
     // glc.size(400, 400);
     // glc.setDuration(5);
     // glc.setFPS(20);
-    glc.setMode("single");
+    glc.setMode("bounce");
     glc.setEasing(true);
     // glc.setMaxColors(256);
     var list = glc.renderList,
@@ -12,31 +12,23 @@ function onGLC(glc) {
         color = glc.color;
 
     // your code goes here:
-    list.addLine({
-        x0: 0,
-        y0: [0,400],
-        x1: [0,400],
-        y1: 400
+    for(i=0;i<400;i+=20){
+        list.addLine({
+            x0: 0,
+            y0: [i,400],
+            x1: [i,400],
+            y1: 400
+        });
+        list.addLine({
+            x0: 400,
+            y0: [400-i,000],
+            x1: [400-i,000],
+            y1: 0
+        });
+    };
+    list.addCircle({
+        x: 200,
+        y: 200,
+        radius: [50,150]
     });
-    list.addLine({
-        x0: [0,400],
-        y0: 400,
-        x1: 400,
-        y1: [400,0]
-    });
-    list.addLine({
-        x0: 400,
-        y0: [400,0],
-        x1: [400,0],
-        y1: 0
-    });
-    list.addLine({
-        x0: [400,0],
-        y0: 0,
-        x1: 0,
-        y1: [0,400]
-    })
-
-
-
 }
